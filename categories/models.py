6a58b78b44c827a70b120e4c325e8be734ee3d16
Adriana <adrianacds92@gmail.com>
2022-11-01 14:53:36 -0300

    feat: criar app addresses e model

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
+
 class Categorie(models.Model):
     id = models.IntegerField()
     color = models.CharField(max_length=20, unique=True)
     description = models.CharField(max_length=255)
-
