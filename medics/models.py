99f6889fdb52c2c677c50dc492f5da006f07a005
Adriana <adrianacds92@gmail.com>
2022-11-01 22:58:57 -0300

    feat: criar app employees e medics e model

---
+++ medics/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Medic(models.Model):
+    crm = models.CharField(unique=True)
+
+    address = models.ForeignKey(
+        "addresses.Address",
+        on_delete=models.CASCADE,
+        related_name="medics",
+    )
+
+    category = models.ForeignKey(
+        "categories.Category",
+        on_delete=models.CASCADE,
+        related_name="medics",
+    )
+
+    specialty = models.ForeignKey(
+        "specialties.Specialty",
+        on_delete=models.CASCADE,
+        related_name="medics",
+    )
+
+    account = models.ForeignKey(
+        "accounts.Account",
+        on_delete=models.CASCADE,
+        related_name="medics",
+    )
