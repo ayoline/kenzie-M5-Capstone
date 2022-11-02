4a77674718796668293fa5523a6cbebd8d567103
ayoline <ayo@outlook.com.br>
2022-11-02 18:18:01 -0300

    feat: add urls.py and some views config on medics app

--- medics/views.py
+++ medics/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Medic
+from .serializers import MedicSerializer
 
-# Create your views here.
+
+class MedicView(ListCreateAPIView):
+    queryset = Medic.objects.all()
+    serializer_class = MedicSerializer
