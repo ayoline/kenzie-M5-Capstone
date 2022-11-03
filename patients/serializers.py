ede144d28e9b3eabdfe6e319db4428f82e350241
Anderson Ferreira Alves <andersonferreiraalves007@gmail.com>
2022-11-03 00:17:27 -0300

    feat: serializers do medico

--- patients/serializers.py
+++ patients/serializers.py
@@ -3,6 +3,11 @@ from .models import Patient
 from addresses.serializers import AddressSerializer
 from categories.serializers import CategorySerializer
 from charts.serializers import ChartSerializer
+from accounts.models import Account
+from addresses.models import Address
+from categories.models import Category
+from charts.models import Chart
+from django.shortcuts import get_object_or_404
 
 
 class PatientSerializer(serializers.ModelSerializer):
@@ -23,36 +28,45 @@ class PatientSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        body = { **validated_data }
-        dataGroup = body.pop('group')
-        dataTraits = body.pop('traits')
+        body = {**validated_data}
+        first_name = body.pop("first_name")
+        last_name = body.pop("last_name")
+        phone_number = body.pop("phone_number")
+        username = body.pop("username")
+        password = body.pop("password")
 
-        obj2, created = Group.objects.get_or_create(
-        name=dataGroup['name'],
-        scientific_name=dataGroup['scientific_name'],
+        cpf = body.pop("cpf")
+
+        address_data = body.pop("address") 
+
+        chart_data = body.pop("chart") 
+
+        category_data = body.pop("category")
+
+        category = get_object_or_404(Category, color=category_data)
+
+        account = Account.objects.create_user({
+            "first_name": first_name,
+            "last_name": last_name,
+            "phone_number": phone_number,
+            "username": username,
+            "password": password,
+            "is_medic": False
+        })
+
+        address = Address.objects.create({**address_data})
+
+        chart = Chart.objects.create({**chart_data})
+
+        patient = Patient.objects.create(
+            {"cpf": cpf}, 
+            address=address,
+            category=category,
+            chart=chart,
+            account=account
         )
 
-        traits = []
-
-        for item in dataTraits:
-            obj, created = Trait.objects.get_or_create(
-            name=item['name'],
-            )
-            traits.append(obj)
-
-            animal = Animal.objects.create(**body, group=obj2)
-
-            animal.traits.set(traits)
-
-        return animal
+        return patient
 
     def update(self, instance, validated_data):
-        for key, value in validated_data.items():
-            if key == 'group' or  key == 'traits' or  key == 'sex':
-                message = {f"{key}": f"You can not update {key} property."}
-                raise NotUpdateFieldError(message)
-            setattr(instance, key, value)
-        
-        instance.save()
-
-        return instance
+        ...
