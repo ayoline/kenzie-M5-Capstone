b26a54188a8f3ac15f641b71841ec9aa0088a8c6
Anderson Ferreira Alves <andersonferreiraalves007@gmail.com>
2022-11-03 22:09:26 -0300

    feat: patients and medics

--- medics/views.py
+++ medics/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from .models import Medic
 from .serializers import MedicSerializer
 
@@ -6,3 +7,9 @@ from .serializers import MedicSerializer
 class MedicView(ListCreateAPIView):
     queryset = Medic.objects.all()
     serializer_class = MedicSerializer
+
+
+class MedicDetailsView(RetrieveUpdateDestroyAPIView):
+    queryset = Medic.objects.all()
+    serializer_class = MedicSerializer
+    
--- patients/views.py
+++ patients/views.py
@@ -1,8 +1,26 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.generics import RetrieveAPIView
 from .models import Patient
 from .serializers import PatientSerializer
+from charts.models import Chart
+from charts.serializers import ChartSerializer, ChartPatientSerializer
 
 
 class PatientView(ListCreateAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+
+class PatientDetailsView(RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+class PatientChartView(RetrieveAPIView):
+    queryset = Chart.objects.all()
+    serializer_class = ChartPatientSerializer
+    lookup_url_kwarg = 'patient_id'
+
+    
+    
