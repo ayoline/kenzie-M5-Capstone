8abaf0a835e935502deceaeef1ac0dafd60d522c
ayoline <ayo@outlook.com.br>
2022-11-05 14:55:38 -0300

    feat: business rules, on delete specialty

--- specialties/views.py
+++ specialties/views.py
@@ -1,6 +1,12 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Specialty
+from medics.models import Medic
+from schedules.models import Schedule
+from django.contrib import messages
 from .serializers import SpecialtySerializer
+from django.core.exceptions import PermissionDenied
+from rest_framework import status
+import ipdb
 
 
 class SpecialtyView(ListCreateAPIView):
@@ -12,3 +18,19 @@ class SpecialtyDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = SpecialtySerializer
     queryset = Specialty.objects.all()
     lookup_url_kwarg = "specialty_id"
+
+    def perform_destroy(self, instance):
+        medics = Medic.objects.all()
+        schedule = Schedule.objects.all()
+        clinic_general = "Clínico Geral"
+
+        if len(medics.filter(specialty__id=instance.id)):
+            raise PermissionDenied
+
+        if len(schedule.filter(specialty_id=instance.id, completed=False)):
+            raise PermissionDenied
+
+        if instance.name.__contains__(clinic_general):
+            raise PermissionDenied
+
+        instance.delete()
