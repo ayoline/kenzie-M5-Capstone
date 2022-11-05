f4e208ffa479ecd8ab9299fcb8976d7fed36efac
Emmanuelle Oliveira <emmanuelleamaral@hotmail.com>
2022-11-05 12:26:46 -0300

    feat: add reletionship tests - patients

---
+++ patients/tests/tests_model.py
@@ -0,0 +1,195 @@
+from django.test import TestCase
+from patients.models import Patient
+from categories.models import Category
+from addresses.models import Address
+from charts.models import Chart
+from django.db import IntegrityError
+
+
+class PatientsModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+
+        cls.address_data = {
+            "street": "Rua ValVerde",
+            "number": 30,
+            "cep": "14726826",
+            "state": "MG",
+            "district": "Centro",
+            "city": "Belo Horizonte",
+        }
+        cls.address = Address.objects.create(**cls.address_data)
+
+        cls.category_data = {
+            "color": "verde",
+            "description": "atende pela tarde",
+        }
+        cls.category = Category.objects.create(**cls.category_data)
+
+        cls.chart_data = {
+            "is_pregnant": False,
+            "is_diabetic": True,
+            "is_smoker": True,
+            "is_allergic": True,
+            "heart_disease": False,
+            "dificulty_healing": False,
+            "use_medication": True,
+            "other_information": "Se encontra bem nervoso",
+        }
+        cls.chart = Chart.objects.create(**cls.chart_data)
+
+        cls.patient_data = {
+            "first_name": "Adriana",
+            "last_name": "Silva",
+            "cpf": "13678958236",
+        }
+        cls.patient = Patient.objects.create(**cls.patient_data,
+                                             chart=cls.chart,
+                                             address=cls.address,
+                                             category=cls.category)
+
+    def test_patient_fields(self):
+        self.assertEqual(self.patient.first_name,
+                         self.patient_data["first_name"])
+        self.assertEqual(self.patient.last_name,
+                         self.patient_data["last_name"])
+        self.assertEqual(self.patient.cpf, self.patient_data["cpf"])
+
+    def test_patient_and_first_name(self):
+        """
+        Conferir informações entre patient e first_name
+        """
+        expected_first_name = "Adriana"
+        result_first_name = self.patient.first_name
+        msg = f"Verifique se o valor de `first_name` foi definida como {expected_first_name}"
+
+        self.assertEqual(expected_first_name, result_first_name, msg)
+
+    def test_cpf_max_length(self):
+        """
+        Verifica a propriedade de tamanho máximo de `cpf`
+        """
+        expected_max_length = 14
+        result_max_length = Patient._meta.get_field("cpf").max_length
+        msg = f"Verifique se a propriedade `max_length` de name foi definida como {expected_max_length}"
+
+        self.assertEqual(expected_max_length, result_max_length, msg)
+
+
+class PatientRelationshipTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.address_data = {
+            "street": "Rua ValVerde",
+            "number": 30,
+            "cep": "14726826",
+            "state": "MG",
+            "district": "Centro",
+            "city": "Belo Horizonte",
+        }
+
+        cls.category_data = {
+            "color": "verde",
+            "description": "atende pela tarde",
+        }
+
+        cls.chart_data = {
+            "is_pregnant": False,
+            "is_diabetic": True,
+            "is_smoker": True,
+            "is_allergic": True,
+            "heart_disease": False,
+            "dificulty_healing": False,
+            "use_medication": True,
+            "other_information": "Se encontra bem nervoso",
+        }
+
+        cls.patient_data = {
+            "first_name": "Adriana",
+            "last_name": "Silva",
+            "cpf": "13678958236",
+        }
+
+        cls.chart = Chart.objects.create(**cls.chart_data)
+        cls.address = Address.objects.create(**cls.address_data)
+        cls.category = Category.objects.create(**cls.category_data)
+        cls.patient = Patient.objects.create(
+            **cls.patient_data,
+            chart=cls.chart,
+            address=cls.address,
+            category=cls.category,
+        )
+
+    def test_if_patient_can_have_an_address(self):
+        """
+        Verifica se um patient pode registrar um address relacionado
+        """
+
+        self.assertIs(self.address.patients, self.patient)
+
+        self.assertIs(self.address, self.patient.address)
+
+    def test_if_patient_can_have_a_chart(self):
+        # Verifica se um paciente pode ter um chart relacionado
+
+        self.assertIs(self.chart.patients, self.patient)
+
+        self.assertIs(self.chart, self.patient.chart)
+
+    def test_if_raise_error_when_patient_already_have_a_chart(self):
+        """ Verifica se um erro é levantado ao atribuir o mesmo address a 
+        outro patient """
+
+        with self.assertRaises(IntegrityError):
+            chart_two_data = {
+                "is_pregnant": False,
+                "is_diabetic": True,
+                "is_smoker": True,
+                "is_allergic": True,
+                "heart_disease": False,
+                "dificulty_healing": False,
+                "use_medication": True,
+                "other_information": "Se encontra bem nervoso",
+            }
+
+            patient_two_data = {
+                "first_name": "Maria",
+                "last_name": "Silva",
+                "cpf": "13678958235",
+            }
+
+            chart_two = Chart.objects.create(**chart_two_data)
+            patient_two = Patient.objects.create(**patient_two_data,
+                                                 chart=chart_two,
+                                                 address=self.address,
+                                                 category=self.category,
+                                                 )
+
+    def test_if_raise_error_when_patient_already_have_a_chart(self):
+        """ Verifica se um erro é levantado ao atribuir o mesmo chart a 
+        outro patient """
+
+        with self.assertRaises(IntegrityError):
+            address_two_data = {
+                "street": "rua das margaridas",
+                "number": 30,
+                "cep": "12345656",
+                "state": "SP",
+                "district": "bairro lalala",
+                "city": "São Paulo",
+            }
+
+            patient_two_data = {
+                "first_name": "Mari",
+                "last_name": "Silva",
+                "cpf": "13678958245",
+            }
+
+            address_two = Address.objects.create(**address_two_data)
+
+            patient_two = Patient.objects.create(
+                **patient_two_data,
+                chart=self.chart,
+                address=address_two,
+                category=self.category,
+            )
