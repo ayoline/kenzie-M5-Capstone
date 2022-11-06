41ce07c9fb87493fd0bdc470f8d5bd6a9d61b742
ayoline <ayo@outlook.com.br>
2022-11-06 13:04:14 -0300

    feat: add permissions to category route

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 from django.urls import path
 
 urlpatterns = [
-    # Acessa o download do schema
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Opcionais
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
